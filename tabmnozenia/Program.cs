e4ab892448a556352bdec0d1bde8442fbe19b5f2
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-04-15 07:37:39 +0200

    Add project files.

---
+++ tabmnozenia/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace tabmnozenia 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tabliczby = new int[11, 11];
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    tabliczby[i, j] = j*i;
+                }
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write("{0,3:D}", tabliczby[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
