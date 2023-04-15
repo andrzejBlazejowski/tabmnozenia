5c42e6fb7f85e019b6c56f7da13f9ca477d2bc62
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-04-15 07:51:39 +0200

    zadanie 1.2

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,40 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Zadanie_1_2();
+        }
 
+        static void Zadanie_1_1()
+        {
+            Console.WriteLine("podaj tekst do wypisania : ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("wprowadziłeś następujący tekst : ");
+            Console.WriteLine(input);
+        }
+
+        static void Zadanie_1_2()
+        {
+            Console.WriteLine("Podaj wartość w km/h, jaką chcesz przeliczyć na m/s");
+
+            string consoleInput = Console.ReadLine();
+
+            double kmPerH;
+
+            if (double.TryParse(consoleInput, out kmPerH))
+            {
+                Console.WriteLine("Przeliczona wartość wynosi : {0}", convertKmPerHToMPerS(kmPerH));
+            }
+            else
+            {
+                Console.WriteLine("Niestety wprowadziłeś niepoprawne dane");
+            }
+
+
+        }
+        static double convertKmPerHToMPerS(double kmPerH)
+        {
+            return kmPerH * 0.277778;
         }
     }
 }
