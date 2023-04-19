8dbaa60fea6205b336ab79dfdc6824ef9e86121a
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-04-19 21:29:27 +0200

    wektor v2

--- Lab2/wektor/Wektor.cs
+++ Lab2/wektor/Wektor.cs
@@ -11,7 +11,23 @@ namespace Lab2.wektor
     {
         private int n;
         private double[] wspolrzedne;
-
+        private static char znakWypisywania;
+        private static Random random = new Random();
+        public Wektor(int n, double a, double b)
+        {
+            this.n = n;
+            this.wspolrzedne = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                wspolrzedne[i] = random.NextDouble() * (b - a) + a;
+            }
+        }
+        static Wektor()
+        {
+            Console.Write("Podaj znak wypisywania wektorów: ");
+            znakWypisywania = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+        }
         public Wektor(int n, double[] dane)
         {
             this.n = n;
@@ -21,14 +37,34 @@ namespace Lab2.wektor
                 wspolrzedne[i] = dane[i];
             }
         }
+        public Wektor(double[] dane)
+        {
+            this.n = dane.Length;
+            this.wspolrzedne = dane;
+        }
+        public static Wektor zbudujWektor()
+        {
+            Console.Write("Wpisz ilość współrzędnych wektora: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double[] dane = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Wpisz wartość współrzędnej nr {0}: ", i + 1);
+                dane[i] = double.Parse(Console.ReadLine());
+            }
+
+            return new Wektor(dane);
+        }
 
         public void show()
         {
-            Console.Write("wspolrzedne wektora to: ");
+            Console.Write("wspolrzedne wektora to: "+ znakWypisywania);
             for (int i = 0; i < n; i++)
             {
                 Console.Write(wspolrzedne[i] + (i != n - 1 ? ", " : ""));
             }
+            Console.Write(znakWypisywania);
             Console.WriteLine();
         }
 
@@ -41,7 +77,6 @@ namespace Lab2.wektor
             }
             return suma;
         }
-
         public Wektor add(Wektor scalar)
         {
             if (n != scalar.n)
