00cbe930f0409ef73476cbd463137de304329f73
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-06-02 12:33:40 +0200

    lab 5 1st

---
+++ Lab5/Dane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Dane<T>
+    {
+        private T dane;
+
+        public T Pole
+        {
+            get { return dane; }
+            set { dane = value; }
+        }
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public void WypiszDane(T argument)
+        {
+            Console.WriteLine("Aktualny typ: {0}", typeof(T).Name);
+            Console.WriteLine("Wartość pola: {0}", Pole);
+            Console.WriteLine("Wartość właściwości: {0}", Pole);
+            Console.WriteLine("Argument metody: {0}", argument);
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab5;
+
+Console.WriteLine("Hello, World!");
+
+
+Dane<string> daneString = new Dane<string>();
+daneString.Pole = "Przykładowy tekst";
+daneString.WypiszDane("Argument dla string");
+
+Dane<int> daneInt = new Dane<int>();
+daneInt.Pole = 42;
+daneInt.WypiszDane(123);
+
+Dane<double> daneDouble = new Dane<double>();
+daneDouble.Pole = 3.14;
+daneDouble.WypiszDane(2.71);
