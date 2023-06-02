202cbc66c80c3c71c6d60d7c8747af7063584da6
andrzejBlazejowski <andrzej.blazejowski@gmail.com>
2023-06-02 12:40:30 +0200

    nexytsfdsdf

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,17 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 using Lab5;
 
-Console.WriteLine("Hello, World!");
 
 
-Dane<string> daneString = new Dane<string>();
-daneString.Pole = "Przykładowy tekst";
-daneString.WypiszDane("Argument dla string");
+class Program
+{
+    static void Main(string[] args)
+    {
 
-Dane<int> daneInt = new Dane<int>();
-daneInt.Pole = 42;
-daneInt.WypiszDane(123);
+        Console.WriteLine("Hello, World!");
 
-Dane<double> daneDouble = new Dane<double>();
-daneDouble.Pole = 3.14;
-daneDouble.WypiszDane(2.71);
+
+        Dane<string> daneString = new Dane<string>();
+        daneString.Pole = "Przykładowy tekst";
+        daneString.WypiszDane("Argument dla string");
+
+        Dane<int> daneInt = new Dane<int>();
+        daneInt.Pole = 42;
+        daneInt.WypiszDane(123);
+
+        Dane<double> daneDouble = new Dane<double>();
+        daneDouble.Pole = 3.14;
+        daneDouble.WypiszDane(2.71);
+
+        Osoba[] osoby = new Osoba[]
+        {
+            new Osoba { Imie = "Jan", Nazwisko = "Kowalski", Wiek = 30 },
+            new Osoba { Imie = "Anna", Nazwisko = "Nowak", Wiek = 25 },
+            new Osoba { Imie = "Piotr", Nazwisko = "Wójcik", Wiek = 40 }
+        };
+
+        Zestawienie<Osoba> zestawienieOsob = new Zestawienie<Osoba>(osoby);
+        zestawienieOsob.GenerujZestawienie();
+
+        Console.WriteLine();
+
+        Produkt[] produkty = new Produkt[]
+        {
+            new Produkt { Nazwa = "Telewizor", Cena = 1999.99m, StanMagazynowy = 10 },
+            new Produkt { Nazwa = "Smartfon", Cena = 999.99m, StanMagazynowy = 5 },
+            new Produkt { Nazwa = "Laptop", Cena = 2999.99m, StanMagazynowy = 3 }
+        };
+
+        Zestawienie<Produkt> zestawienieProduktow = new Zestawienie<Produkt>(produkty);
+        zestawienieProduktow.GenerujZestawienie();
+
+        Console.ReadLine();
+    }
+}
+
+class Osoba
+{
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public int Wiek { get; set; }
+}
+
+class Produkt
+{
+    public string Nazwa { get; set; }
+    public decimal Cena { get; set; }
+    public int StanMagazynowy { get; set; }
+}
---
+++ Lab5/Zestawnienie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5
+{
+
+    public class Zestawienie<T>
+    {
+        private T[] dane;
+
+        public Zestawienie(T[] dane)
+        {
+            this.dane = dane;
+        }
+
+        public void GenerujZestawienie()
+        {
+            if (dane.Length == 0)
+            {
+                Console.WriteLine("Brak danych ");
+                return;
+            }
+
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.Write("{0,-15}", property.Name);
+            }
+            Console.WriteLine();
+
+            foreach (var element in dane)
+            {
+                foreach (var property in properties)
+                {
+                    Console.Write("{0,-15}", property.GetValue(element));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
